ff56ac37db29c5e456c91288e253502241d45a7b
Ortigoza, Matías Nicolás <ortigozamatias@gmail.com>
2021-01-20 21:31:36 -0300

    react-fundamentals 07

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -4,23 +4,23 @@
 import * as React from 'react'
 
 const allItems = [
-  {id: 'apple', value: '🍎 apple'},
-  {id: 'orange', value: '🍊 orange'},
-  {id: 'grape', value: '🍇 grape'},
-  {id: 'pear', value: '🍐 pear'},
+  { id: 'apple', value: '🍎 apple' },
+  { id: 'orange', value: '🍊 orange' },
+  { id: 'grape', value: '🍇 grape' },
+  { id: 'pear', value: '🍐 pear' },
 ]
 
-function App() {
+function App () {
   const [items, setItems] = React.useState(allItems)
 
-  function addItem() {
+  function addItem () {
     setItems([
       ...items,
-      allItems.find(i => !items.map(({id}) => id).includes(i.id)),
+      allItems.find(i => !items.map(({ id }) => id).includes(i.id)),
     ])
   }
 
-  function removeItem(item) {
+  function removeItem (item) {
     setItems(items.filter(i => i.id !== item.id))
   }
 
@@ -29,10 +29,10 @@ function App() {
       <button disabled={items.length >= allItems.length} onClick={addItem}>
         add item
       </button>
-      <ul style={{listStyle: 'none', paddingLeft: 0}}>
+      <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
